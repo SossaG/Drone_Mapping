18840fd2a43ab3b1abe5b58bdd5b44a86997914a
Ivan SG <22964938@student.uwa.edu.au>
2025-09-15 12:31:06 +0800

    latest changes

--- src/orbslam3_ros2/launch/orbslam3_ros2.launch.py
+++ src/orbslam3_ros2/launch/orbslam3_ros2.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
     slam_pkg_path = get_package_share_directory("orbslam3_ros2")
 
     vocab_file = os.path.join(slam_pkg_path, "config", "ORBvoc.txt")
-    settings_file = os.path.join(slam_pkg_path, "config", "my_mono_cam.yaml")
+    settings_file = os.path.join(slam_pkg_path, "config", "rpi_cam_mono.yaml")
 
     print(f"Path of vocab file: {vocab_file}")
     print(f"Path of settings file: {settings_file}")
